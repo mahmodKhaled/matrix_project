7b30083b9d630390a8204e63d872bb32d298e925
mahmoudkahled <54672453+mahmoudkahled@users.noreply.github.com>
2020-05-24 19:50:46 +0200

    Delete show_matrix_value.h

--- new_matrix/show_matrix_value.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include<ctype.h>
-void show_matrix(double matrix[3][3]) {
-    cout << matrix[0][0] << " " << matrix[0][1] << " " << matrix[0][2] << endl;
-    cout << matrix[1][0] << " " << matrix[1][1] << " " << matrix[1][2] << endl;
-    cout << matrix[2][0] << " " << matrix[2][1] << " " << matrix[2][2] << endl;
-}
