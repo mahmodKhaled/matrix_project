89fc407d3487dac44d4b2106a84f88a8d6d52ae8
mahmoudkahled <54672453+mahmoudkahled@users.noreply.github.com>
2020-05-24 19:58:10 +0200

    Add files via upload

---
+++ new_matrix/operations_view.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include<ctype.h>
+using namespace std;
+// the next function display the operations that our program do to the user
+void show_operations() {
+    cout << "You can do any operation from the following:" << endl;
+    cout << "write your input without spaces..." << endl;
+    cout << "Show Matrix Value: A or B" << endl;
+    cout << "Addition: C = A + B" << endl;
+    cout << "Subtraction: C = A - B" << endl;
+    cout << "Multiplication: C = A * B" << endl;
+    cout << "Transpose: C = A’" << endl;
+    cout << "Division: C = A/B" << endl;
+}
+
