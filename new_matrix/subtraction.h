875b74755d49dc4f291f4c87fb2c0a225113e03e
mahmoudkahled <54672453+mahmoudkahled@users.noreply.github.com>
2020-05-24 19:50:39 +0200

    Delete subtraction.h

--- new_matrix/subtraction.h
+++
@@ -1,7 +0,0 @@
-void subtract_two_matrices(double matrix1[3][3], double matrix2[3][3], double result_matrix[3][3]) {
-    for (int j = 0; j < 3; j++) {
-        for (int k = 0; k < 3; k++) {
-            result_matrix[j][k] = matrix1[j][k] - matrix2[j][k];
-        }
-    }
-}
