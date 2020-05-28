ea6cab23eea8a130e5bdd546977228159c45e1f0
mahmoudkahled <54672453+mahmoudkahled@users.noreply.github.com>
2020-05-28 20:04:53 +0200

    Add files via upload

--- new_matrix/new_matrix.cpp
+++ new_matrix/new_matrix.cpp
@@ -8,6 +8,8 @@
 #include "addition.h"
 #include "subtraction.h"
 #include "transpose.h"
+#include "multiplication.h"
+#include "division.h"
 using namespace std;
 int main()
 {
@@ -15,75 +17,123 @@ int main()
     double matrix1[3][3]{}, matrix2[3][3]{}, result_matrix[3][3]{};
     vector<double> fill_matrix{}, vec1{}, vec2{}, vec3{};
     string data{}, sum_numbers{}, choice{};
-    int num_of_matrices{}, found{};
+    int found{};
     double number{}, result{}, detereminant{};
-    cout << "Enter the number of matrices: ";
-    cin >> num_of_matrices;
-    cin.ignore(); // take number of matrices from the user
-    for (int i = 0; i < num_of_matrices; i++) {
-        cout << "Enter the contents of the matrix: ";
-        getline(cin, data); // take the input from the user
-        found = data.find('.');
+    while (true) {
+        for (int i = 0; i < 2; i++) {
+            cout << "Enter the contents of the matrix: ";
+            getline(cin, data); // take the input from the user
+            found = data.find('.');
+            if (data == "exit") {
+                break;
+            }
+            else {
+                operations_in_input(data, sum_numbers, fill_matrix, number, found);
+                separate_fill_matrix_into_three_vectors(vec1, vec2, vec3, fill_matrix);
+                if (i == 0) {
+                    fill_vector2D(mat1, vec1, vec2, vec3);
+                    fill_array2D(matrix1, mat1);
+                    // this if condition fill matrix No 1
+                }
+                if (i == 1) {
+                    fill_vector2D(mat2, vec1, vec2, vec3);
+                    fill_array2D(matrix2, mat2);
+                    // this if condition fill matrix No 2
+                }
+                empty_vectors(vec1, vec2, vec3, fill_matrix);
+            }
+        }
         if (data == "exit") {
             break;
         }
-        else {
-            operations_in_input(data, sum_numbers, fill_matrix, number, found);
-            separate_fill_matrix_into_three_vectors(vec1, vec2, vec3, fill_matrix);
-            if (i == 0) {
-                fill_vector2D(mat1, vec1, vec2, vec3);
-                fill_array2D(matrix1, mat1);
-                // this if condition fill matrix No 1
+        while (true) {
+            show_operations();
+            cout << "Enter your choice here: ";
+            cin >> choice;
+            cin.ignore();
+            if (choice == "leave") {
+                break;
             }
-            if (i == 1) {
-                fill_vector2D(mat2, vec1, vec2, vec3);
-                fill_array2D(matrix2, mat2);
-                // this if condition fill matrix No 2
+            else if (choice[0] == 'A') {
+                show_matrix(matrix1);
+                detereminant = DET(matrix1);
+                cout << "the determinant of the matrix is: " << detereminant << endl;
+                cout << "---------------------" << endl;
             }
-            empty_vectors(vec1, vec2, vec3, fill_matrix);
-        }
-    }
+            else if (choice[0] == 'B') {
+                show_matrix(matrix2);
+                detereminant = DET(matrix2);
+                cout << "the determinant of the matrix is: " << detereminant << endl;
+                cout << "---------------------" << endl;
+            }
+            if (choice == "C=A+B") {
+                add_two_matrices(matrix1, matrix2, result_matrix);
+                show_matrix(result_matrix);
+                cout << "---------------------" << endl;
+            }
+            else if (choice == "C=A-B") {
+                subtract_two_matrices(matrix1, matrix2, result_matrix);
+                show_matrix(result_matrix);
+                cout << "---------------------" << endl;
+            }
+            else if (choice == "C=B-A") {
+                subtract_two_matrices(matrix2, matrix1, result_matrix);
+                show_matrix(result_matrix);
+                cout << "---------------------" << endl;
+            }
+            else if (choice == "C=A*B") {
+                multiply_two_matrices(matrix1, matrix2, result_matrix);
+                show_matrix(result_matrix);
 
-    while (true) {
-        show_operations();
-        cin >> choice;
-        if (choice == "exit") {
-            break;
-        }
-        else if (choice[0] == 'A') {
-            show_matrix(matrix1);
-            detereminant = DET(matrix1);
-            cout << "the determinant of the matrix is: " << detereminant << endl;
-            cout << "---------------------" << endl;
-        }
-        else if (choice[0] == 'B') {
-            show_matrix(matrix2);
-            detereminant = DET(matrix2);
-            cout << "the determinant of the matrix is: " << detereminant << endl;
-            cout << "---------------------" << endl;
-        }
-        if (choice == "C=A+B") {
-            add_two_matrices(matrix1, matrix2, result_matrix);
-            show_matrix(result_matrix);
-            cout << "---------------------" << endl;
-        }
-        else if (choice == "C=A-B") {
-            subtract_two_matrices(matrix1, matrix2, result_matrix);
-            show_matrix(result_matrix);
-            cout << "---------------------" << endl;
-        }
-        else if (choice == "C=A'") {
-            transpose_matrix(matrix1,result_matrix);
-            show_matrix(result_matrix);
-            cout << "---------------------" << endl;
-        }
-        else if (choice == "C=B'") {
-            transpose_matrix(matrix2, result_matrix);
-            show_matrix(result_matrix);
-            cout << "---------------------" << endl;
+            }
+            else if (choice == "C=B*A") {
+                multiply_two_matrices(matrix2, matrix1, result_matrix);
+                show_matrix(result_matrix);
+            }
+            else if (choice == "C=A'") {
+                transpose_matrix(matrix1, result_matrix);
+                show_matrix(result_matrix);
+                cout << "---------------------" << endl;
+            }
+            else if (choice == "C=B'") {
+                transpose_matrix(matrix2, result_matrix);
+                show_matrix(result_matrix);
+                cout << "---------------------" << endl;
+
+            }
+            else if (choice == "C=A`") {
+                double detereminant = DET(matrix1);
+                if (detereminant == 0) {
+                    cout << "Can't get the inverse" << endl;
+                    continue;
+                }
+                inv(matrix1, result_matrix);
+                show_matrix(result_matrix);
+            }
+            else if (choice == "C=B`") {
+                double detereminant = DET(matrix2);
+                if (detereminant == 0) {
+                    cout << "Can't get the inverse" << endl;
+                    continue;
+                }
+                inv(matrix2, result_matrix);
+                show_matrix(result_matrix);
+            }
+            else if (choice == "C=A/B") {
+                double detereminant = DET(matrix2);
+                if (detereminant == 0) {
+                    cout << "Can't get the inverse" << endl;
+                    continue;
+                }
+                inv(matrix2, result_matrix);
+                multiply_two_matrices(matrix1, result_matrix, matrix2);
+                show_matrix(matrix2);
+            }
+            else if (choice == "C=B/A") {
+                
+                show_matrix(result_matrix);
+            }
         }
     }
     return 0;
 }
-
-
